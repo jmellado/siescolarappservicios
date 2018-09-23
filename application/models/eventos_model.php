44b7dc20d9f37f81e49e52163a046668010efd34
jmellado <je_in_er@hotmail.com>
2018-09-23 14:41:57 -0500

    Version 2.1
    - Modificacion de las funciones buscar y detalle de mensajes, tareas y
    eventos.

--- application/models/eventos_model.php
+++ application/models/eventos_model.php
@@ -11,9 +11,10 @@ class Eventos_model extends CI_Model {
 
 		$this->db->order_by('fecha_envio', 'desc');
 
-		$this->db->join('personas', 'notificaciones.remitente = personas.id_persona');
+		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
+		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_envio,estado_lectura,personas.nombres,personas.apellido1,personas.apellido2');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
 
 		$query = $this->db->get('notificaciones');
 
@@ -26,9 +27,10 @@ class Eventos_model extends CI_Model {
 
 		$this->db->where('id_notificacion',$id_notificacion);
 
-		$this->db->join('personas', 'notificaciones.remitente = personas.id_persona');
+		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
+		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_inicio,hora_inicio,fecha_fin,hora_fin,fecha_envio,estado_lectura,personas.nombres,personas.apellido1,personas.apellido2');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_inicio,hora_inicio,fecha_fin,hora_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
 
 		$query = $this->db->get('notificaciones');
 
