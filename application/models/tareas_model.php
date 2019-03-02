42ec14be538970941b8246d922fe37eb59cab5d8
jmellado <je_in_er@hotmail.com>
2019-03-02 18:07:05 -0500

    Version 2.3
    - Modificacion de la funcion buscar_tarea() y detalle_tarea().

--- application/models/tareas_model.php
+++ application/models/tareas_model.php
@@ -14,7 +14,7 @@ class Tareas_model extends CI_Model {
 		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
 		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
 
 		$query = $this->db->get('notificaciones');
 
@@ -30,7 +30,7 @@ class Tareas_model extends CI_Model {
 		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
 		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,fecha_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,fecha_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
 
 		$query = $this->db->get('notificaciones');
 
