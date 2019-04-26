7703da64de8398a19c4a2c66fa7613b4695052c3
jmellado <je_in_er@hotmail.com>
2019-04-25 21:22:02 -0500

    Version 2.7
    - Modificaciones en la funcion buscar_tarea() y detalle_tarea().

--- application/models/tareas_model.php
+++ application/models/tareas_model.php
@@ -13,8 +13,9 @@ class Tareas_model extends CI_Model {
 
 		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
 		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
+		$this->db->join('asignaturas', 'notificaciones.id_asignatura = asignaturas.id_asignatura');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,notificaciones.id_asignatura,fecha_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est,nombre_asignatura');
 
 		$query = $this->db->get('notificaciones');
 
@@ -29,8 +30,9 @@ class Tareas_model extends CI_Model {
 
 		$this->db->join('personas as pf', 'notificaciones.remitente = pf.id_persona');
 		$this->db->join('personas as est', 'notificaciones.id_estudiante = est.id_persona');
+		$this->db->join('asignaturas', 'notificaciones.id_asignatura = asignaturas.id_asignatura');
 
-		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,fecha_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est');
+		$this->db->select('id_notificacion,codigo_notificacion,categoria_notificacion,remitente,titulo,tipo_notificacion,contenido,destinatario,rol_destinatario,id_estudiante,notificaciones.id_asignatura,fecha_fin,fecha_envio,estado_lectura,pf.nombres,pf.apellido1,pf.apellido2,est.nombres as nombresest,est.apellido1 as apellido1est,est.apellido2 as apellido2est,nombre_asignatura');
 
 		$query = $this->db->get('notificaciones');
 
